61a92448c7af1faee06490b7f9b0e4e07032cc4b
Tony Misic <tony.misic@ryerson.ca>
2021-03-10 15:03:38 -0500

    loss code complete and needs to be validated, training code started

--- debug.py
+++ debug.py
@@ -9,7 +9,7 @@ CLIP, preprocess = clip.load("RN50", device=device)
 CLIP.visual = our_model
 CLIP.type(torch.FloatTensor).to(device)
 # our implementation of the loss
-def clip_loss(video_feature_batch, text_feature_batch, batch_size, temperature):
+def clip_loss(video_feature_batch, text_feature_batch, batch_size, temperature=0.07): # temperature is learned ?
     # l2 normalization
     video_feature_batch = video_feature_batch / video_feature_batch.norm(dim=-1, keepdim=True)
     text_feature_batch = text_feature_batch / text_feature_batch.norm(dim=-1, keepdim=True)
@@ -18,34 +18,29 @@ def clip_loss(video_feature_batch, text_feature_batch, batch_size, temperature):
     logits_per_video = logit_scale * video_feature_batch @ text_feature_batch.t()
     logits_per_text = logit_scale * text_feature_batch @ video_feature_batch.t()
     # cross entropy loss
-    labels = torch.eye(batch_size).type(torch.LongTensor).to(device)
-    #### Doesn't work
     loss = torch.nn.CrossEntropyLoss()
-    label = torch.Tensor([1.]).type(torch.LongTensor).to(device)
-    video_loss = loss(logits_per_video, label)
-    text_loss = loss(logits_per_text, labels[:, 0:])
+    # generate labels
+    labels = torch.arange(batch_size).type(torch.LongTensor).to(device)
+    # calculate loss of mini-batch
+    video_loss = loss(logits_per_video, labels)
+    text_loss = loss(logits_per_text, labels)
     overall_loss = (video_loss + text_loss) / 2
-    #### Doesn't work
     return video_loss, text_loss, overall_loss
 
 # load example video and text
 video = torch.from_numpy(vl.norm(vl.random_temporal_crop(vl.center_spatial_crop(vl.read_video('dog.mp4'), 224, 224), 64)))
 video = video.unsqueeze_(0)
 video = video.permute([0,4,1,2,3]).type(torch.half).to(device)
-text = clip.tokenize(["a dog"]).to(device)
-with torch.no_grad():
-    video_features = CLIP.encode_video(video)
-    text_features = CLIP.encode_text(text)
-    logits_per_image, logits_per_text = CLIP(video, text)
-    probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-
-clip_loss(video_features, text_features, 1, 2)
-# debug our model
-# model = network.generate_model(50)
-# model.load_state_dict(torch.load('./3dRN50_CLIP.pth'))
-# x = torch.rand(1,3,64,224,224) # batch, chann, time, hieght, weidth
-# o = model(x)
-# print(o.size())
+text = clip.tokenize(["a dog", "a pet", "a cat"]).to(device)
+videos = torch.Tensor().to(device)
+for i in range(3):
+    with torch.no_grad():
+        video_features = CLIP.encode_video(video)
+        videos = torch.cat([videos, video_features], 0)
+        text_features = CLIP.encode_text(text)
+        #logits_per_image, logits_per_text = CLIP(video, text)
+        #probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+video_loss, text_loss, overall_loss = clip_loss(videos, text_features, 3)
 
 
 
